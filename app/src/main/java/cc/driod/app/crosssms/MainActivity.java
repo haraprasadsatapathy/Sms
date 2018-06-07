dfacded3a6c970e67162aca50505db50175f33a4
Haraprasad <haraprasadsatapathy@gmail.com>
2018-06-07 11:26:20 +0530

    CreateProject07062018

--- app/src/main/java/cc/driod/app/crosssms/MainActivity.java
+++ app/src/main/java/cc/driod/app/crosssms/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//Change
+//Change   hc
 
     }
 
