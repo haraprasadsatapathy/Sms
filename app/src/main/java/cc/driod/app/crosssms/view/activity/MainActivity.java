60d44063a265770df62ddd4a2125181c266d84ff
Swagatika <swagatika.panda.756@gmail.com>
2018-06-07 12:19:05 +0530

    //kkfjgjflgjfkg

--- app/src/main/java/cc/driod/app/crosssms/view/activity/MainActivity.java
+++ app/src/main/java/cc/driod/app/crosssms/view/activity/MainActivity.java
@@ -1,13 +1,8 @@
 package cc.driod.app.crosssms.view.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import cc.driod.app.crosssms.R;
 
@@ -19,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //kkfjgjflgjfkg
 
 
     }
